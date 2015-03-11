c2fd6e4060a9f50de5d85709390c33d11bfd3a78
nohyesung <nxsung@naver.com>
2015-03-11 22:31:44 +0900

    2번째 commit

--- testProject/src/SE/testProject/testClass.java
+++ testProject/src/SE/testProject/testClass.java
@@ -3,6 +3,6 @@ package SE.testProject;
 public class testClass {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world");
+		System.out.println("hello world - recomit");
 	}
 }
