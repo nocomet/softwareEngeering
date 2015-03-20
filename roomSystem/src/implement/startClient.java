fbc07708a41fae270c0d06c0f9d464bc9edb57b4
nohyesung <nxsung@naver.com>
2015-03-20 14:15:35 +0900

    implement package의 startServer 실행 - startClient 실행
    startServer부터 객체들을 따라가서 주석 확인 해볼 것
    
    -노혜성

--- roomSystem/src/implement/startClient.java
+++ roomSystem/src/implement/startClient.java
@@ -13,8 +13,11 @@ public class startClient {
 	public startClient()
 	{
 		client=new Client("127.0.0.1", 11114);
+		
+		//GUI의 총 관리하는 FramePage class 할당	
 		nomal_FramePage frame = new nomal_FramePage();
 		
+		//GUI에 해당하는 각 기능을 가진 class 할당
 		gui=GUI_console.getInstance();
 		gui.setClient(client);
 		gui.setFrame(frame);		
