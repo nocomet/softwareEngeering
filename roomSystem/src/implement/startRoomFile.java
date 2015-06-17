d163a0763c9aa388da304019c00f6525e28ff726
nohyesung <nxsung@naver.com>
2015-06-17 22:52:29 +0900

    [문서]15.06.17 최종보고서

--- roomSystem/src/implement/startRoomFile.java
+++ roomSystem/src/implement/startRoomFile.java
@@ -13,10 +13,10 @@ public class startRoomFile{
 		roomFile file=new roomFile();				
 		
 		// 회의실명 , 시, 구, 수용인원, 값, 소유자명, email
-		list.addRoom(new conferenceRoom("자취방 회의실", "대구 광역시", "북구", "30", "10", "노혜성","nxsung@naver.com",3,new ImageIcon("./src/사진1.jpg")));		
+		list.addRoom(new conferenceRoom("자취방 회의실", "대구 광역시", "북구", "30", "10", "노혜성","nxsung@naver.com",3,new ImageIcon("./src/회의실01.png")));		
 		list.addRoom(new conferenceRoom("도서관", "대구 광역시", "동구", "20", "10", "노혜성","nxsung@naver.com",4,new ImageIcon("./src/room1.png")));		
 		list.addRoom(new conferenceRoom("대구 메르스 대피소", "대구 광역시", "남구", "25", "10", "노혜성","nxsung@naver.com",5,new ImageIcon("./src/room2.png")));		
-		list.addRoom(new conferenceRoom("경산의 땅", "대구 광역시", "달성구", "25", "15", "이신영","lsy8795@gmail.com",6,new ImageIcon("./src/사진2.jpg")));		
+		list.addRoom(new conferenceRoom("경산의 땅", "대구 광역시", "달성구", "25", "15", "이신영","lsy8795@gmail.com",6,new ImageIcon("./src/회의실02.png")));		
 		list.addRoom(new conferenceRoom("기숙사 휴게소", "대구 광역시", "중구", "20", "10", "이신영","lsy8795@gmail.com",7,new ImageIcon("./src/room2.png")));		
 		list.addRoom(new conferenceRoom("부산 메르스 대피소", "부산 광역시", "동구", "20", "10", "이신영","lsy8795@gmail.com",8,new ImageIcon("./src/room1.png")));		
 		list.addRoom(new conferenceRoom("메스르 병원_회의실", "서울 특별시", "강남구", "20", "10", "이흔정","oooyat@naver.com",0, new ImageIcon("./src/room1.png")));		
